2a4e83260e167bdf12ade1589dd11ee85fb8f6fa
GMoliang <1784084633@qq.com>
2020-06-17 09:13:40 +0800

    郭沫良的第一次作业

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -8,10 +8,29 @@ package day03;
  **/
 public class HomeWork {
     //打印九九乘法表
-
+    public static void jiujiu(){
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+"*"+i+"="+i*j+" ");
+            }
+            System.out.println();
+        }
+    }
     //打印等腰三角形
-
+    public static void dengyao(){
+        int n=5;
+        for(int i=0;i<n;i++){
+           for(int j=0;j<n-i;j++){
+               System.out.print(" ");
+           }
+           for(int j=0;j<=2*i;j++){
+               System.out.print("*");
+           }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        jiujiu();
+        dengyao();
     }
 }
