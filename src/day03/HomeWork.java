0d1989dd86e9b7e32dc69d7c66c600b8a218bafe
GMoliang <1784084633@qq.com>
2020-06-17 08:47:10 +0800

    郭沫良作业第一次项目结构上传

---
+++ src/day03/HomeWork.java
@@ -0,0 +1,17 @@
+package day03;
+
+/**
+ * @program: gmlhomework
+ * @description: 今天的作业
+ * @author: Mr.Gml
+ * @create: 2020-06-17 08:41
+ **/
+public class HomeWork {
+    //打印舅舅乘法表
+
+    //打印等腰三角形
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
